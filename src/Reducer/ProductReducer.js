ea02e4828bacbad13aa075b0e2e2a65949155680
prashantmestry <mestry.prashant10@gmail.com>
2023-08-16 21:14:59 +0530

    remove cart

--- src/Reducer/ProductReducer.js
+++ src/Reducer/ProductReducer.js
@@ -1,10 +1,13 @@
 
 const reducer = (state,action) =>{
+    console.log('action' , action);
     switch(action.type){
         case 'ADD_PRODUCT':
            return { ...state , productList : action.data } 
         case 'ADD_IN_CARTS':
             return { ...state , carts : [ ...state.carts , action.data] }
+        case 'DELETE_FROM_CART':
+                return { ...state , carts : state.carts.filter(item => item.id !== action.data.id) }            
         default :
             return state;
     }
--- src/component/Products.js
+++ src/component/Products.js
@@ -1,85 +1,126 @@
-import React, { useContext, useEffect, useReducer, useState } from 'react';
-import productReducer from '../Reducer/ProductReducer';
-import axios from 'axios';
-import { Button } from '@mui/material';
+import React, { useContext, useEffect, useReducer, useState } from "react";
+import productReducer from "../Reducer/ProductReducer";
+import axios from "axios";
 
-const initialState ={
-    productList :[],
-    carts:[]
-}
+const initialState = {
+  productList: [],
+  carts: [],
+};
 
-const Products = (props) =>{
-    
-    const [state,dispatch] = useReducer(productReducer,initialState);
+const Products = (props) => {
+  const [state, dispatch] = useReducer(productReducer, initialState);
 
-    useEffect(() =>{
-        let productsUrl = 'https://fakestoreapi.com/products';
-        axios.get(productsUrl).then(res =>{
-            console.log('res',res.data);
-            dispatch({
-                type : 'ADD_PRODUCT',
-                data : res.data
-            })
-        }).catch(err =>{
-            console.log('error',err);
-        })
-
-    },[]);
-
-    const addToCart = (item) =>{
-        console.log('item',item);
+  useEffect(() => {
+    let productsUrl = "https://fakestoreapi.com/products";
+    axios
+      .get(productsUrl)
+      .then((res) => {
+        console.log("res", res.data);
         dispatch({
-            type : 'ADD_IN_CARTS',
-            data: item
-        })
-    }
+          type: "ADD_PRODUCT",
+          data: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  }, []);
 
+  const addToCart = (item) => {
+    dispatch({
+      type: "ADD_IN_CARTS",
+      data: item,
+    });
+  };
 
-    return(
-        <div  className="text-3xl font-bold">
+  const totalCost = (list) =>{
+    return list.reduce((acc , item ) =>{
+        return acc + (item.price)
+    },0)
+  }
+
+  return (
+    <div className="text-3xl font-bold">
+        <div className="flex justify-between">
             <h1>Products</h1>
-            {
-                (state.carts.length > 0) &&
-                <div className='flex border-2 my-4 p-4'>
-                    {
-                        state.carts.map(val =>{
-                            return (
-                            <div className='flex flex-col mr-8'>
-                                <div className='flex'><img width={50} height={50} src={ val.image } /></div>
-                                <div className='text-sm '>{val.price}</div>
-                            </div>
-                            )
-                        })
-                    }
-                </div>
-            }
-            <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-4 text-lg">
-            {
-                state.productList.map(val =>{
-                    return (
-                        <div className="p-4 border-2" key={val.id}>
-                            <div className='m-4'>{val.title}</div>
-                            <div className='m-2 text-sm'>{val?.description.length > 200 ? val.description.substr( 0, 200)+"..." : val.description}</div>
-                            <div className='m-2 flex flex-col text-center'>
-                                <div><img src={val.image} width={100} height={100} /></div>
-                                <div className='text-center'>{val.price}</div>
-                            </div>        
-                            {
-                                //state.carts.find(item => item.id === val.id) &&
-                                <Button className="bg-slate-400" onClick={() => addToCart({
-                                    id : val.id,
-                                    image : val.image,
-                                    price :val.price,
-                                    title:val.title
-                                })}>Add To Cart</Button>      
-                            }              
-                        </div>
-                    )
-                })
-            }
-            </div>
+            <div className="text-sm">{ state.carts.length > 0 ? '$'+totalCost(state.carts) : 'Cart is empty'}</div>
         </div>
-    )
-}
+      {state.carts.length > 0 && (
+        <div className="flex gap-4 p-4 my-4 bg-slate-100 overflow-x-auto">
+          {state.carts.map((val) => {
+            return (
+              <div className="flex flex-col text-center justify-between p-2 bg-white">
+                <div className="flex justify-center">
+                  <img width={50} height={50} src={val.image} />
+                </div>
+                <div className="text-sm mt-3">${val.price}</div>
+                <button
+                    className="bg-red-300 px-4 py-2 text-sm"
+                  onClick={() => {
+                    dispatch({
+                      type: "DELETE_FROM_CART",
+                      data: { id: val.id },
+                    });
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 text-lg">
+        {state.productList.map((val) => {
+          return (
+            <div className="p-4 border-2 flex flex-col justify-between" key={val.id}>
+              <div className="">{val.title}</div>
+                <div className="grid grid-cols-2 mt-4">
+                    <div className="p-2 text-center">
+                        <div className="flex justify-center text-center col-span-2"><img src={val.image} width={100} height={100} /></div>
+                        <div className="mt-2">${val.price}</div>
+                    </div>
+                    <div className="m-2 text-sm whitespace-pre-wrap">
+                        {val?.description.length > 200
+                        ? val.description.substr(0, 200) + "..."
+                        : val.description}
+                    </div>
+                </div> 
+                            
+                <div className="text-center ">
+                    {
+                        state.carts.some((item) => item.id == val.id) ? (
+                        <button
+                        className="bg-red-300 mt-4 px-6 py-2 text-sm"
+                        onClick={() => dispatch({
+                            type: "DELETE_FROM_CART",
+                            data: { id: val.id },
+                        })}
+                        >
+                        Remove from cart
+                        </button>
+                    ) : (
+                        <button
+                        className="bg-green-400 mt-4 px-6 py-2 text-sm"
+                        onClick={() =>
+                            addToCart({
+                            id: val.id,
+                            image: val.image,
+                            price: val.price,
+                            title: val.title,
+                            })
+                        }
+                        >
+                        Add To Cart
+                        </button>
+                    )}
+                </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Products;
